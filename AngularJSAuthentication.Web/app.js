f59e01dfaf814062cc700ca377b18a4f420f2c37
Young <wyoung@proforma.com>
2018-05-02 08:55:50 -0400

    Finished

---
+++ AngularJSAuthentication.Web/app.js
@@ -0,0 +1,107 @@
+﻿var app = angular.module('AngularAuthApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "/app/views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: "/app/views/signup.html"
+    });
+
+    $routeProvider.when("/orders", {
+        controller: "ordersController",
+        templateUrl: "/app/views/orders.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+});
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
+
+'use strict';
+app.factory('authService', ['$http', '$q', 'localStorageService', function ($http, $q, localStorageService) {
+
+    var serviceBase = 'http://ngauthenticationapi.azurewebsites.net/';
+    var authServiceFactory = {};
+
+    var _authentication = {
+        isAuth: false,
+        userName: ""
+    };
+
+    var _saveRegistration = function (registration) {
+
+        _logOut();
+
+        return $http.post(serviceBase + 'api/account/register', registration).then(function (response) {
+            return response;
+        });
+
+    };
+
+    var _login = function (loginData) {
+
+        var data = "grant_type=password&username=" + loginData.userName + "&password=" + loginData.password;
+
+        var deferred = $q.defer();
+
+        $http.post(serviceBase + 'token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).success(function (response) {
+
+            localStorageService.set('authorizationData', { token: response.access_token, userName: loginData.userName });
+
+            _authentication.isAuth = true;
+            _authentication.userName = loginData.userName;
+
+            deferred.resolve(response);
+
+        }).error(function (err, status) {
+            _logOut();
+            deferred.reject(err);
+        });
+
+        return deferred.promise;
+
+    };
+
+    var _logOut = function () {
+
+        localStorageService.remove('authorizationData');
+
+        _authentication.isAuth = false;
+        _authentication.userName = "";
+
+    };
+
+    var _fillAuthData = function () {
+
+        var authData = localStorageService.get('authorizationData');
+        if (authData) {
+            _authentication.isAuth = true;
+            _authentication.userName = authData.userName;
+        }
+
+    }
+
+    app.config(function ($httpProvider) {
+        $httpProvider.interceptors.push('authInterceptorService');
+    });
+
+    authServiceFactory.saveRegistration = _saveRegistration;
+    authServiceFactory.login = _login;
+    authServiceFactory.logOut = _logOut;
+    authServiceFactory.fillAuthData = _fillAuthData;
+    authServiceFactory.authentication = _authentication;
+
+    return authServiceFactory;
+}]);
